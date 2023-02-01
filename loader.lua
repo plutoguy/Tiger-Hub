51b4dd01bf6664b70f6381acb8eb6cf40d4ab3d6
plutoguy <123932868+plutoguy@users.noreply.github.com>
2023-02-01 20:52:20 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 1962086868 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/plutoguy/Tiger-Hub/main/tower%20of%20hell.lua"))();
+elseif game.PlaceId == 286090429 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/plutoguy/Tiger-Hub/main/arsenal.lua"))();
 end  
