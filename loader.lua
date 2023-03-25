d54dcf629facb9646bf7486bce34d2db139bbf26
plutoguy <123932868+plutoguy@users.noreply.github.com>
2023-03-25 11:40:55 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,9 @@
 if game.PlaceId == 1962086868 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/plutoguy/Tiger-Hub/main/tower%20of%20hell.lua"))();
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/plutoguy/Tiger-Hub/main/tower%20of%20hell.lua"))()
 elseif game.PlaceId == 286090429 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/plutoguy/Tiger-Hub/main/arsenal.lua"))();
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/plutoguy/Tiger-Hub/main/arsenal.lua"))()
 elseif game.PlaceId == 662417684 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/plutoguy/Tiger-Hub/main/lucky%20blocks%20battleground.lua"))();
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/plutoguy/Tiger-Hub/main/lucky%20blocks%20battleground.lua"))()
+elseif game.PlaceId == 2753915549 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/plutoguy/Tiger-Hub/main/bloxfruits.lua"))()
 end  
