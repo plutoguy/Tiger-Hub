3b225063a06ffa1d51ad079cf063d90800d15f2d
plutoguy <123932868+plutoguy@users.noreply.github.com>
2024-07-23 11:07:16 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,6 +4,6 @@ elseif game.PlaceId == 286090429 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/plutoguy/Tiger-Hub/main/arsenal.lua"))()
 elseif game.PlaceId == 662417684 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/plutoguy/Tiger-Hub/main/lucky%20blocks%20battleground.lua"))()
-elseif game.PlaceId == 2753915549 or 4442272183 or 7449423635 then
+elseif game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/plutoguy/Tiger-Hub/main/bloxfruits.lua"))()
 end  
